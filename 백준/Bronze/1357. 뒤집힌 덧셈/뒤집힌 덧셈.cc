b8acaec23fd27e767bd9de295d855cc6c81dfa77
김준호 <92168240+kokohuhusave@users.noreply.github.com>
2023-10-27 19:39:25 +0900

    [Bronze I] Title: 뒤집힌 덧셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1357. 뒤집힌 덧셈/뒤집힌 덧셈.cc
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+int rev(int n)
+    {
+        int sum = 0;
+        while (n)
+        {
+            sum = sum * 10 + n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+int main()
+{
+        int x, y;
+        cin >> x >> y;
+
+        cout << rev(rev(x) + rev(y));
+
+
+}
