86fa6d8a3e6b72b8880c699d8458f6d27ccf96e3
김준호 <92168240+kokohuhusave@users.noreply.github.com>
2023-11-27 16:33:43 +0900

    [Bronze I] Title: 팰린드롬수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <algorithm> 
+using namespace std;
+int main() {
+    string N;
+
+
+    while (N != "0") {
+        cin >> N;
+        string M = N;
+        reverse(N.begin(), N.end()); 
+        if (N == "0") { 
+            break;
+        }
+        else if (N == M) { 
+            cout << "yes\n";
+        }
+        else {
+            cout << "no\n";
+        }
+    }
+}
