6c87165ae604bf7e1ca47f3ecd426da8b5a48d07
김준호 <92168240+kokohuhusave@users.noreply.github.com>
2025-04-29 11:18:11 +0900

    [Bronze II] Title: 나머지, Time: 100 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,24 +1,20 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-        int count = 0; 
-        int value[] = new int[10];
-        
-        for(int i = 0;i < 10;i++) {
-        	int N = Integer.parseInt(br.readLine());
-        	value[i] = N % 42;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < 10; i++) {
+            int N = Integer.parseInt(br.readLine());
+            set.add(N % 42);
         }
-        
-        value = Arrays.stream(value).distinct().toArray();
-        System.out.println(value.length);
-	}
 
+        System.out.println(set.size());
+    }
 }
-
