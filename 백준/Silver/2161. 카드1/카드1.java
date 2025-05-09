e665c5f45703cb6814fbf6a3622b5fcdbdef64d7
김준호 <92168240+kokohuhusave@users.noreply.github.com>
2025-05-09 10:21:17 +0900

    [Silver V] Title: 카드1, Time: 140 ms, Memory: 14956 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.java
@@ -0,0 +1,33 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		Queue<Integer> queue = new LinkedList<>();
+		
+		for(int i = 1;i <= N;i++) {
+			queue.offer(i);
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		
+		while(queue.size() > 1) {
+				System.out.print(queue.poll() + " ");
+				int temp = queue.poll();
+				queue.offer(temp);
+			
+		}
+		System.out.println(queue.poll());
+	}
+
+}
